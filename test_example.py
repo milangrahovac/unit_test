d606a14fee2c13c31f32754b0caff1210c8a0845
Milan Grahovac <grahovac85@gmail.com>
2018-06-25 20:00:34 +0200

    2nd commit

--- test_example.py
+++ test_example.py
@@ -2,10 +2,7 @@ import unittest
 from example import lettersCombinations
 
 
-
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
         self.assertAlmostEqual(lettersCombinations(""), [])
         self.assertAlmostEqual(lettersCombinations("23"), ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
-
-
